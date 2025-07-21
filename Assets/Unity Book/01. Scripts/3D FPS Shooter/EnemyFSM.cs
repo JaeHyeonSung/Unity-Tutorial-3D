34b535048d4754edf28965666fa04062b04fbf9f
JaeHyeonSung <top91652@gmail.com>
2025-07-21 10:28:27 +0900

    EnemyFSM

---
+++ Assets/Unity Book/01. Scripts/3D FPS Shooter/EnemyFSM.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyFSM : MonoBehaviour
+{
+    enum EnemyState
+    {
+        Idle,
+        Move,
+        Attack,
+        Return,
+        Damaged,
+        Die
+    }
+
+    EnemyState m_State;
+    Transform player;
+    CharacterController cc;
+
+
+    public float findDistance = 8.0f;  // 탐지 가능 거리
+    public float attackDistance = 3f;  // 공격 가능 거리
+    public float moveSpeed = 5f;       // 이동 속도
+    public float currentTime = 0f;     // 시간
+    public float attackDelay = 2f;     // 공격 딜레이
+    public int attackPower = 3;        // 공격력
+    public int hp = 15;                // 체력
+    private Vector3 originPos;         // 원래 위치
+    public float moveDistance = 20f;   // 추격 가능 거리
+    private void Start()
+    {
+        m_State = EnemyState.Idle;
+
+        player = GameObject.Find("Player").transform;
+        cc = GetComponent<CharacterController>();
+
+        originPos = transform.position;
+    }
+    private void Update()
+    {
+        switch (m_State)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.Damaged:
+                //Damaged();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
+    private void Idle()
+    {
+        if (Vector3.Distance(transform.position, player.position) < findDistance)
+        {
+            m_State = EnemyState.Move;
+            Debug.Log("상태전환: Idle->Move");
+        }
+    }
+    private void Move()
+    {
+        if(Vector3.Distance(transform.position, originPos) > moveDistance)
+        {
+            m_State = EnemyState.Return;
+            Debug.Log("상태 전환: Move->Return");
+        }
+        else if (Vector3.Distance(transform.position, player.position) > attackDistance)
+        {
+            Vector3 dir = (player.position - transform.position).normalized;
+
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_State=EnemyState.Attack;
+            Debug.Log("상태 전환: Move -> Attack");
+
+            currentTime = attackDelay;
+        }
+    }
+    private void Attack()
+    {
+        if(Vector3.Distance(transform.position, player.position) < attackDistance)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime > attackDelay)
+            {
+                player.GetComponent<FPSPlayerMove>().DamageAction(attackPower);
+                Debug.Log("공격");
+                currentTime = 0;
+            }
+        }
+        else
+        {
+            m_State = EnemyState.Move;
+            Debug.Log("상태 전환: Attack->Move");
+            currentTime = 0;
+        }
+    }
+
+    private void Return()
+    {
+        if (Vector3.Distance(transform.position, originPos) > 0.1f)
+        {
+            Vector3 dir = (originPos - transform.position).normalized;
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            transform.position = originPos;
+            hp = 15;
+            m_State = EnemyState.Idle;
+            Debug.Log("상태 전환: Return->Idle");
+        }
+    }
+
+    private void Damaged()
+    {
+        StartCoroutine(DamageProcess());
+    }
+
+    private void Die()
+    {
+
+    }
+
+    IEnumerator DamageProcess()
+    {
+        yield return new WaitForSeconds(0.5f);
+        m_State = EnemyState.Move;
+        Debug.Log("상태 전환: Damaged->Move");
+    }
+    public void HitEnemy(int hitPower)
+    {
+        hp-=hitPower;
+        if(hp > 0)
+        {
+            m_State = EnemyState.Damaged;
+            Debug.Log("상태 전환 : AnyState -> Damaged");
+            Damaged();
+        }
+        else
+        {
+            m_State = EnemyState.Die;
+            Debug.Log("상태 전환 : AnyState->Die");
+            Die();
+        }
+    }
+}
---
+++ Assets/Unity Book/01. Scripts/3D FPS Shooter/FPSPlayerFire.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class FPSPlayerFire : MonoBehaviour
+{
+    public GameObject firePosition;
+    public GameObject bombFactory;
+
+    public GameObject bulletEffect;
+
+    ParticleSystem ps;
+
+    public int weaponPower = 5;
+    private void Start()
+    {
+        ps = bulletEffect.GetComponent<ParticleSystem>();
+    }
+
+
+    public float throwPower =15f;
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.AddForce(Camera.main.transform.forward * throwPower ,ForceMode.Impulse);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            RaycastHit hitInfo = new RaycastHit();
+
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                {
+                    EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
+                    eFSM.HitEnemy(weaponPower);
+                }
+                else
+                {
+                    bulletEffect.transform.position = hitInfo.point;
+
+                    bulletEffect.transform.forward = hitInfo.normal;
+                    ps.Play();
+                }
+                
+
+                
+            }
+        }
+    }
+}
